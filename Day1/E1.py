a0cbc4fb7da415df826f0d22c1647a26b0a9e9da
Stallion1618 <rayoshock@gmail.com>
2021-11-15 18:45:27 -0600

    What I made in day 1

---
+++ Day1/E1.py
@@ -0,0 +1,20 @@
+#string
+string1 = "hello world"
+#integer
+num1 = 3
+#float
+float1 = 3.1416
+#Boolean
+flag = True
+flag2 = False
+
+"""print (f"Integer = {num1}")
+print ("Flag1 = {flag1}")
+string3 = input ("Type a new string:   ")
+print(string2)"""
+
+a = int(input ("Type a number:   "))
+b = int(input("Type another number:   "))
+c = a ** b
+print(c)
+
---
+++ Day1/E2.py
@@ -0,0 +1,7 @@
+#Band Name Generator
+print("Welcome to the band name generator")
+city = input ("What's the name of the city where you grew up in?   ")
+pet= input ("What's your pet's name?:   ")
+
+band_name = city + " " + pet
+print(f "Your fucking band name could be {band_name}")
